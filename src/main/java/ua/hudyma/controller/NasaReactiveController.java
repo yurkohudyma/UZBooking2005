6e07cd8d3ebee5a492cb0a966ba6dc42287f842d
yurkohudyma <hudyma@gmail.com>
2025-10-04 10:10:46 +0300

    Largest Photo FIXED

--- src/main/java/ua/hudyma/controller/NasaReactiveController.java
+++ src/main/java/ua/hudyma/controller/NasaReactiveController.java
@@ -1,19 +1,31 @@
 package ua.hudyma.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import ua.hudyma.service.NasaService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/nasa")
 public class NasaReactiveController {
     private final NasaService nasaService;
 
-    @GetMapping public Mono<String> getLargestPhotoUrl (){
+    @GetMapping(value = "/nasa", produces = MediaType.IMAGE_PNG_VALUE)
+    public Mono<byte[]> getLargestPhotoUrl (){
+        return nasaService
+                .getLargestPhotoUrl()
+                .flatMap(url -> WebClient.create(url)
+                        .mutate().codecs(config -> config.defaultCodecs().maxInMemorySize(10_000_000)).build()
+                        .get()
+                        .exchangeToMono(resp -> resp.bodyToMono(byte[].class))
+                );
+    }
+
+    @GetMapping("/url")
+    public Mono<String> getUrl (){
         return nasaService.getLargestPhotoUrl();
     }
 }
--- src/main/java/ua/hudyma/service/NasaService.java
+++ src/main/java/ua/hudyma/service/NasaService.java
@@ -9,22 +9,31 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 @Service
 public class NasaService {
     public Mono<String> getLargestPhotoUrl() {
-        return WebClient.create("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=DEMO_KEY")
+        return WebClient.create("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=Scg2TSwDMS7WtHRjfASWj7hcGycNiayWUNaKnFRL")
                 .get()
                 .exchangeToMono(resp -> resp.bodyToMono(JsonNode.class))
                 .map(json -> json.get("photos"))
                 .flatMapMany(Flux::fromIterable)
                 .map(photo -> photo.get("img_src"))
                 .map(JsonNode::asText)
-                .flatMap(redirectedPhotoUrl -> WebClient.create(redirectedPhotoUrl)
-                        .get()
+                .flatMap(photoUrl -> WebClient.create(photoUrl)
+                        .head()
                         .exchangeToMono(ClientResponse::toBodilessEntity)
                         .map(HttpEntity::getHeaders)
-                        .map(HttpHeaders::getContentLength)
-                        .map(size -> new Photo(redirectedPhotoUrl, size)
+                        .map(HttpHeaders::getLocation)
+                        .map(URI::toString)
+                                .flatMap(redirectedPhotoUrl -> WebClient.create(redirectedPhotoUrl)
+                                        .head()
+                                        .exchangeToMono(ClientResponse::toBodilessEntity)
+                                        .map(HttpEntity::getHeaders)
+                                        .map(HttpHeaders::getContentLength)
+                                        .map(size -> new Photo (redirectedPhotoUrl, size)
+                                )
                         )
                 )
                 .reduce((p1, p2) -> (p1.size > p2.size()) ? p1 : p2)
